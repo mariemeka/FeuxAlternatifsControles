25f41a405bfb12851cb13a00635ab43eacd00b2e
mariemeka <marieme1212@gmail.com>
2024-03-14 11:40:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/feux/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.feux;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.buttonSignal);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
+
